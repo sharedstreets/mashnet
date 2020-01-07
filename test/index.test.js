511ff2937c4bcf51abe1bbd466d70b72836bcc90
morganherlocker <morgan.herlocker@gmail.com>
2020-01-06 16:06:31 -0800

    match classifier training and evaluation

--- test/index.test.js
+++ test/index.test.js
@@ -23,15 +23,17 @@ test("mashnet", async t => {
   };
   var result = net.match(addition);
 
-  addition.properties.stroke = "blue";
+  t.ok(result.length > 0, "found matches");
+  t.equal(result[0].line.type, "Feature", "result contains matched feature");
+
+  /*addition.properties.stroke = "blue";
   result[0].line.properties.stroke = "red";
   console.log(
     JSON.stringify(turf.featureCollection([addition, result[0].line]))
   );
   console.log(JSON.stringify(result));
   console.log("SCORE: ", result[0].softmax);
-  console.log(result);
-  t.ok(result.length > 0, "found matches");
+  console.log(result);*/
 
   t.done();
 });
